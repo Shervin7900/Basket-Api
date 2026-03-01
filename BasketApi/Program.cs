7cf508e5a4241e499cf594f03fc5aee9581ac153
shervinfadaee@yahoo.com <shervinfadaee7900@gmail.com>
2026-03-01 15:46:08 +0100

    feat: Add Basket API with integrated observability using Grafana, Prometheus, Loki, and Vector.

--- BasketApi/Program.cs
+++ BasketApi/Program.cs
@@ -3,8 +3,18 @@ using BasketApi.Domain.Interfaces;
 using BasketApi.Infrastructure.Persistence;
 using FastEndpoints;
 using FastEndpoints.Swagger;
+using Sentry;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Sentry (error tracking + tracing)
+builder.WebHost.UseSentry(o =>
+{
+    o.Dsn = builder.Configuration["Sentry:Dsn"] ?? "";
+    o.TracesSampleRate = double.TryParse(builder.Configuration["Sentry:TracesSampleRate"], out var tsr) ? tsr : 0.2;
+    o.ProfilesSampleRate = double.TryParse(builder.Configuration["Sentry:ProfilesSampleRate"], out var psr) ? psr : 0.1;
+    o.SendDefaultPii = false;
+});
 
 // Add BaseInfrastructure (Redis, Health, etc. from BaseApi submodule)
 builder.Services.AddBaseInfrastructure(builder.Configuration);
